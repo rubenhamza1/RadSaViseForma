2ac27f26dc4c965e25ccec000ec44d0726889ccd
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-09-26 12:39:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ViseFormi
+{
+    partial class formStudenti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDodajNovog = new System.Windows.Forms.Button();
+            this.btnObrisiSve = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnDodajNovog
+            // 
+            this.btnDodajNovog.Location = new System.Drawing.Point(21, 12);
+            this.btnDodajNovog.Name = "btnDodajNovog";
+            this.btnDodajNovog.Size = new System.Drawing.Size(139, 23);
+            this.btnDodajNovog.TabIndex = 1;
+            this.btnDodajNovog.Text = "Dodaj novog studenta";
+            this.btnDodajNovog.UseVisualStyleBackColor = true;
+            this.btnDodajNovog.Click += new System.EventHandler(this.btnDodajNovog_Click);
+            // 
+            // btnObrisiSve
+            // 
+            this.btnObrisiSve.Location = new System.Drawing.Point(21, 259);
+            this.btnObrisiSve.Name = "btnObrisiSve";
+            this.btnObrisiSve.Size = new System.Drawing.Size(139, 23);
+            this.btnObrisiSve.TabIndex = 2;
+            this.btnObrisiSve.Text = "Obrisi sve studente";
+            this.btnObrisiSve.UseVisualStyleBackColor = true;
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(363, 127);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(105, 23);
+            this.btnObrisi.TabIndex = 3;
+            this.btnObrisi.Text = "Obrisi studenta";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(336, 199);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // formStudenti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 303);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnObrisiSve);
+            this.Controls.Add(this.btnDodajNovog);
+            this.Name = "formStudenti";
+            this.Text = "Studenti";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnDodajNovog;
+        private System.Windows.Forms.Button btnObrisiSve;
+        private System.Windows.Forms.Button btnObrisi;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ViseFormi
+{
+    partial class formUnosNovogStudenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.cmbSmjer = new System.Windows.Forms.ComboBox();
+            this.btnSnimi = new System.Windows.Forms.Button();
+            this.txtBrojIndeksa = new System.Windows.Forms.TextBox();
+            this.btnOtkazi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDatumRodenja = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(12, 44);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(200, 20);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(12, 118);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(200, 20);
+            this.txtPrezime.TabIndex = 1;
+            // 
+            // cmbSmjer
+            // 
+            this.cmbSmjer.FormattingEnabled = true;
+            this.cmbSmjer.Items.AddRange(new object[] {
+            "Programer",
+            "Sistem administrator",
+            "Mrežni administrator"});
+            this.cmbSmjer.Location = new System.Drawing.Point(12, 187);
+            this.cmbSmjer.Name = "cmbSmjer";
+            this.cmbSmjer.Size = new System.Drawing.Size(200, 21);
+            this.cmbSmjer.TabIndex = 2;
+            // 
+            // btnSnimi
+            // 
+            this.btnSnimi.Location = new System.Drawing.Point(13, 263);
+            this.btnSnimi.Name = "btnSnimi";
+            this.btnSnimi.Size = new System.Drawing.Size(75, 23);
+            this.btnSnimi.TabIndex = 3;
+            this.btnSnimi.Text = "Snimi";
+            this.btnSnimi.UseVisualStyleBackColor = true;
+            this.btnSnimi.Click += new System.EventHandler(this.btnSnimi_Click);
+            // 
+            // txtBrojIndeksa
+            // 
+            this.txtBrojIndeksa.Location = new System.Drawing.Point(257, 44);
+            this.txtBrojIndeksa.Name = "txtBrojIndeksa";
+            this.txtBrojIndeksa.Size = new System.Drawing.Size(200, 20);
+            this.txtBrojIndeksa.TabIndex = 4;
+            // 
+            // btnOtkazi
+            // 
+            this.btnOtkazi.Location = new System.Drawing.Point(382, 263);
+            this.btnOtkazi.Name = "btnOtkazi";
+            this.btnOtkazi.Size = new System.Drawing.Size(75, 23);
+            this.btnOtkazi.TabIndex = 6;
+            this.btnOtkazi.Text = "Otkazi";
+            this.btnOtkazi.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Smjer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Broj indeksa:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(254, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Datum rođenja:";
+            // 
+            // dtpDatumRodenja
+            // 
+            this.dtpDatumRodenja.Location = new System.Drawing.Point(257, 115);
+            this.dtpDatumRodenja.Name = "dtpDatumRodenja";
+            this.dtpDatumRodenja.Size = new System.Drawing.Size(200, 20);
+            this.dtpDatumRodenja.TabIndex = 12;
+            // 
+            // formUnosNovogStudenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 301);
+            this.Controls.Add(this.dtpDatumRodenja);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOtkazi);
+            this.Controls.Add(this.txtBrojIndeksa);
+            this.Controls.Add(this.btnSnimi);
+            this.Controls.Add(this.cmbSmjer);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Name = "formUnosNovogStudenta";
+            this.Text = "Unos novog studenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.ComboBox cmbSmjer;
+        private System.Windows.Forms.Button btnSnimi;
+        private System.Windows.Forms.TextBox txtBrojIndeksa;
+        private System.Windows.Forms.Button btnOtkazi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDatumRodenja;
+    }
+}
